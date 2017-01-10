6f5adc54bcc420d4b4c218299fd3fb22399e326a
Sidredin Agashirinov <sidredin@inbox.ru>
2017-01-10 08:06:27 +0300

    yandex-verification added

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -18,6 +18,7 @@ AppAsset::register($this);
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="yandex-verification" content="acbf5de5088307a9" />
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
